9e09abb4623ce625ad07d5de1e2d89807ee2f62a
Jorge Munera <gerjo9211@hotmail.com>
2025-06-25 13:35:04 -0500

    feat: creamos nuestro array de propductos en memoria

--- src/servicios.js
+++ src/servicios.js
@@ -1,1 +1,27 @@
-// CRUD
+// ENTIDAD PRODUCTOS
+
+// precio, descripcion, foto, id, nombre, stock
+
+
+let product1 = {
+    id: 1,
+    name: "juguete",
+    foto: "url.com",
+    stock: 10,
+    precio: 100,
+    descripcion: "url.com",
+    isActivo: false
+}
+
+// lista o un array de varios productos
+
+let productosInMemoria = []; // simulando la base datos
+console.log("1. lista de productos: ", productosInMemoria);
+
+// agregar items a un array
+productosInMemoria.push(product1);
+
+console.log("2. lista de productos: ", productosInMemoria);
+
+
+
