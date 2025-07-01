040fd0691c198ad4a5cb246afb526a312bcaacae
Jorge Munera <gerjo9211@hotmail.com>
2025-07-01 13:46:29 -0500

    feat: crea el servicio de crear productos

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
 // VAMOS A USAR EL SERVICIO
+const {create, productDB} = require("./servicios");
+
+// crear productos
+create({
+    name: "juguete2",
+    foto: "url2.com",
+    stock: 12,
+    precio: 200,
+    descripcion: " descripcion 2",
+    isActive: true
+});
+console.log("db despues del primer create: ", productDB);
+
+// obtener todos los productos
+// getAll();
+
--- src/servicios.js
+++ src/servicios.js
@@ -1,27 +1,49 @@
 // ENTIDAD PRODUCTOS
 
+// Propiedades:
 // precio, descripcion, foto, id, nombre, stock
-
+let nextId = 1;
 
 let product1 = {
-    id: 1,
+    id: nextId,
     name: "juguete",
     foto: "url.com",
     stock: 10,
     precio: 100,
-    descripcion: "url.com",
+    descripcion: "descripcion 1",
     isActivo: false
 }
 
-// lista o un array de varios productos
 
-let productosInMemoria = []; // simulando la base datos
-console.log("1. lista de productos: ", productosInMemoria);
+// lista o un array de varios productos
+let productDB = []; // simulando la base datos
 
 // agregar items a un array
-productosInMemoria.push(product1);
+productDB.push(product1);
 
-console.log("2. lista de productos: ", productosInMemoria);
+// CRUD
+// CREATE, READ, UPDATE, DELETE
 
+    // name: "juguete2",
+    // foto: "url2.com",
+    // stock: 12,
+    // precio: 200,
+    // descripcion: "descripcion 2",
+    // isActivo: false
 
+function create(dataProduct) {
+    const id = nextId + 1;
+    const newProduct = {...dataProduct, id: id};
+    productDB.push(newProduct);
+
+    return newProduct;
+};
+
+// crear la funcion de READ (getAll)
+
+module.exports = {
+    create,
+    productDB,
+    nextId
+}
 
